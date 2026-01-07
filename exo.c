a13dd3a5acf20f9bd21905d9128a3a133c9ad756
yanis akrout <105445785+yayaakt43@users.noreply.github.com>
2026-01-07 19:59:46 +0100

    Update exo.c

--- exo.c
+++ exo.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <signal.h>
 
+void traiter_signal(){
+        printf("Signal Ctrl+C bien reçu !\n");
+    
+}
+
+int main(){
+        signal(SIGINT, traiter_signal);
+        printf("Le Ctrl+c est neutralisé\n");
+    printf("Pour sortir du processus appuyer sur la touche Entrée\n");
+        getchar();
+        printf("Fin du programme\n");
+return 0;}
